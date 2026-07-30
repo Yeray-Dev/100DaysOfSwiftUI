0010456ce9356b70a5759db5d32aa6a1fdf308dc
Yeray-Dev <wow_ybf@hotmail.es>
2026-07-30 20:40:10 +0200

    Project 04: done

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Yeray Blanco de la Fuente on 30/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
